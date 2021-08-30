d6867da28e56f7728be50b76f9a234345523c2ea
ELManioui Soulaimane <elmanioui.soulaimane@gmail.com>
2021-08-30 22:36:59 +0100

    Code

---
+++ index.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    //Generates 4 row sets for the grid layout
+    for (let row = 1; row <= 4; row++) {
+        //Generates 9 pixel columns for the grid layout
+      for (let column = 1; column <= 9; column++) {
+          //Each grid item has a row and column classes that holds their unique position in the grid
+        $(".grid-container").append("<div class='grid-item' row=" +row +" column=" +column +"></div>");
+      }
+    }
+    function changeColor(){
+      //Generate random row index
+      var randomRow = Math.floor(Math.random() * 20);
+      //Generate random column index
+      var randomColumn = Math.floor(Math.random() * 20);
+      //Generate a random color
+      var randomColor = Math.floor(Math.random()*16777215).toString(16);
+      //Fade the selected div
+      $(".grid-item[row='" + randomRow + "'][column='" + randomColumn + "']").fadeOut(3000);
+      //change the background color of the selected div
+      $(".grid-item[row='" + randomRow + "'][column='" + randomColumn + "']").css({'background-color':'#'+randomColor,'filter':blur(5+'px')});
+      //Fade it in!
+      $(".grid-item[row='" + randomRow + "'][column='" + randomColumn + "']").fadeIn(3000);
+    }
+      
+    //Change the delay time for when to call the function as needed below
+    window.setInterval(function(){changeColor();}, 100);  
+  });
+  
